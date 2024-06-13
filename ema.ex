c269469a4c18837070cb8a6fac66606080f02122
Emanuele DelBono <emanuele@codiceplastico.com>
2024-06-13 17:19:24 +0200

    refactoring

--- ema.ex
+++
@@ -1,9 +0,0 @@
-defmodule Receiver do
-  def start do
-    receive do
-      {:say_hello, value} -> IO.puts("Hello " <> value)
-      _ -> IO.puts("Message not supported")
-    end
-    start()
-  end
-end
